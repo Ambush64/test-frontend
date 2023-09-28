40b3bca41b048dbfe82705866ff874fc3d278a9e
Ambush64 <bilal57ei@gmail.com>
2023-09-28 10:38:24 +0530

    fir

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from "./App";
 
 import "./assets/css/app.css";
 import "./assets/css/bootstrap-grid.min.css";
+
+import "./assets/icons/boxicons-2/css/boxicons.min.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
