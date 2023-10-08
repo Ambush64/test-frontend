c0cea0e8589149aa077382a3a6a3c8cef5eeb695
Ambush64 <bilal57ei@gmail.com>
2023-10-08 16:09:45 +0530

    fi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import RegistrationForm from './RegistrationForm';
 import LoginForm from './LoginForm';
@@ -8,14 +8,18 @@ import ViewCart from './ViewCart';
 import ThankYouPage from './ThankYouPage';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/');
+  }, [])
+
   return (
     <div className="App">
       <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
-      <LoginForm />
       <Routes>
         <Route path="/register" element={<RegistrationForm />} />
         <Route path="/" element={<LoginForm />} />
